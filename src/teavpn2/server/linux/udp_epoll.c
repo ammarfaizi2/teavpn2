ammarfaizi2/teavpn2

commit f65243c69a7d6fa0233533400ce709fcf5ecd5ff

server/udp: implement zombie reaper thread
Introducing zombie reaper thread (zr thread).

In order to efficiently reap zombie sessions, we implement zombie
reaper thread which will send request sync packet to authenticated
clients if it reaches the timeout.

It will also close() and free() the sessions those are assumed dead by
the given timeout. The timeout for the authenticated session and
unauthenticated session are different.

For authenticated session, it's `UDP_SESS_TIMEOUT * 2` seconds.
For unauthenticated session, it's `UDP_SESS_TIMEOUT` seconds.

If an authenticated session reaches `UDP_SESS_TIMEOUT`, the zr thread
will send a sync request packet (in hope that the client will reply).
If the client replies, the timeout counter for it will be reset.

Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_epoll.c
@@ -920,6 +920,115 @@ out:
 }
 
 
+static void zombie_chk_authenticated_sess(struct srv_udp_state *state,
+					  struct udp_sess *sess,
+					  time_t time_diff)
+{
+	size_t send_len;
+	struct srv_pkt *srv_pkt = &state->zr.pkt->srv;
+	struct epl_thread *thread = &state->epl_threads[0];
+	const time_t kill_timeout = UDP_SESS_TIMEOUT * 2;
+
+	if (time_diff > kill_timeout) {
+		close_udp_session(&state->epl_threads[0], sess);
+		return;
+	}
+
+	if (time_diff < UDP_SESS_TIMEOUT)
+		return;
+
+	send_len = srv_pprep(srv_pkt, TSRV_PKT_REQSYNC, 0, 0);
+	send_to_client(thread, sess, srv_pkt, send_len);
+}
+
+
+static void zombie_chk_unauthenticated_sess(struct srv_udp_state *state,
+					    struct udp_sess *sess,
+					    time_t time_diff)
+{
+	size_t send_len;
+	struct srv_pkt *srv_pkt = &state->zr.pkt->srv;
+	struct epl_thread *thread = &state->epl_threads[0];
+	const time_t kill_timeout = UDP_SESS_TIMEOUT;
+
+	if (time_diff > kill_timeout) {
+		send_len = srv_pprep(srv_pkt, TSRV_PKT_REQSYNC, 0, 0);
+		send_to_client(thread, sess, srv_pkt, send_len);
+	}
+}
+
+
+static void _run_zombie_reaper_thread(struct srv_udp_state *state)
+{
+	uint16_t i, j, max_conn = state->cfg->sock.max_conn;
+	struct udp_sess *sess, *sess_arr = state->sess_arr;
+
+	for (j = i = 0; i < max_conn; i++) {
+		time_t time_diff;
+
+		sess = &sess_arr[i];
+		if (!atomic_load(&sess->is_connected))
+			continue;
+
+		get_unix_time(&time_diff);
+		time_diff -= sess->last_act;
+
+		if (sess->is_authenticated)
+			zombie_chk_authenticated_sess(state, sess, time_diff);
+		else
+			zombie_chk_unauthenticated_sess(state, sess, time_diff);
+
+		if (atomic_load(&state->n_on_sess) <= ++j)
+			break;
+	}
+}
+
+
+static void *run_zombie_reaper_thread(void *state_p)
+{
+	struct srv_udp_state *state = (struct srv_udp_state *)state_p;
+
+	nice(40);
+	atomic_store(&state->zr.is_online, true);
+
+	state->zr.pkt = calloc_wrp(1ul, sizeof(*state->zr.pkt));
+	if (unlikely(!state->zr.pkt))
+		state->stop = true;
+
+	while (likely(!state->stop)) {
+		_run_zombie_reaper_thread(state);
+		sleep(10);
+	}
+
+	al64_free(state->zr.pkt);
+	atomic_store(&state->zr.is_online, false);
+	return NULL;
+}
+
+
+static int init_zombie_reaper_thread(struct srv_udp_state *state)
+{
+	int ret;
+	pthread_t *zr_thread = &state->zr.thread;
+
+	prl_notice(2, "Spawning zombie reaper thread...");
+	ret = pthread_create(zr_thread, NULL, run_zombie_reaper_thread, state);
+	if (unlikely(ret)) {
+		pr_err("pthread_create(): " PRERF, PREAR(ret));
+		return -ret;
+	}
+
+	pthread_setname_np(*zr_thread, "zombie-reaper");
+	ret = pthread_detach(*zr_thread);
+	if (unlikely(ret)) {
+		pr_err("pthread_detach(): " PRERF, PREAR(ret));
+		return -ret;
+	}
+
+	return ret;
+}
+
+
 static bool wait_for_threads_to_exit(struct srv_udp_state *state)
 {
 	uint8_t nn, i;
@@ -962,6 +1071,31 @@ static bool wait_for_threads_to_exit(struct srv_udp_state *state)
 		if (wait_c++ > 1000)
 			return false;
 	}
+	return true;
+}
+
+
+static bool wait_for_zombie_reaper_thread_to_exit(struct srv_udp_state *state)
+{
+	int ret;
+	unsigned wait_c = 0;
+
+	if (!atomic_load(&state->zr.is_online))
+		return true;
+
+	ret = pthread_kill(state->zr.thread, SIGTERM);
+	if (unlikely(ret)) {
+		pr_err("pthread_kill(state->zr.thread, SIGTERM): " PRERF,
+		       PREAR(ret));
+	}
+
+	prl_notice(2, "Waiting for zombie reaper thread to exit...");
+	while (atomic_load(&state->zr.is_online)) {
+		usleep(100000);
+		if (wait_c++ > 100)
+			return false;
+	}
+
 	return true;
 }
 
@@ -1019,19 +1153,24 @@ static void free_pkt_buffer(struct srv_udp_state *state)
 
 static void destroy_epoll(struct srv_udp_state *state)
 {
-	if (!wait_for_threads_to_exit(state)) {
-		/*
-		 * Thread(s) won't exit, don't free the heap!
-		 */
-		pr_emerg("Thread(s) won't exit!");
-		state->threads_wont_exit = true;
-		return;
-	}
+	if (!wait_for_zombie_reaper_thread_to_exit(state))
+		goto thread_wont_exit;
+
+	if (!wait_for_threads_to_exit(state))
+		goto thread_wont_exit;
 
 	close_epoll_fds(state);
 	close_client_sess(state);
 	free_pkt_buffer(state);
 	al64_free(state->epl_threads);
+	return;
+
+thread_wont_exit:
+	/*
+	 * Thread(s) won't exit, don't free the heap!
+	 */
+	pr_emerg("Thread(s) won't exit!");
+	state->threads_wont_exit = true;
 }
 
 
@@ -1039,11 +1178,16 @@ int teavpn2_udp_server_epoll(struct srv_udp_state *state)
 {
 	int ret;
 
+	state->stop = false;
+	atomic_store(&state->zr.is_online, false);
 	ret = init_epoll_thread_array(state);
 	if (unlikely(ret))
 		goto out;
 
-	state->stop = false;
+	ret = init_zombie_reaper_thread(state);
+	if (unlikely(ret))
+		goto out;
+
 	ret = run_event_loop(state);
 out:
 	destroy_epoll(state);

src/teavpn2/server/linux/udp.h
@@ -17,8 +17,10 @@
 #include <teavpn2/client/common.h>
 
 
-#define EPOLL_EVT_ARR_NUM 3u
-#define UDP_SESS_MAX_ERR 5u
+#define EPOLL_EVT_ARR_NUM	3u
+#define UDP_SESS_MAX_ERR	5u
+#define UDP_SESS_TIMEOUT	30
+
 
 /*
  * UDP session struct.
@@ -117,6 +119,23 @@ struct epl_thread {
 };
 
 
+struct zombie_reaper {
+	/*
+	 * Is zombie reaper online?
+	 */
+	_Atomic(bool)				is_online;
+
+
+	/*
+	 * Zombie reaper thread.
+	 */
+	pthread_t				thread;
+
+
+	struct sc_pkt				*pkt;
+};
+
+
 struct srv_udp_state {
 	/*
 	 * @stop is false when event loop is supposed to run.
@@ -147,7 +166,6 @@ struct srv_udp_state {
 	 * exit, otherwise it's false.
 	 */
 	bool					need_remove_iff;
-
 
 	/*
 	 * @sig should contain signal after signal interrupt
@@ -185,6 +203,9 @@ struct srv_udp_state {
 
 
 	_Atomic(uint16_t)			n_on_threads;
+
+
+	struct zombie_reaper			zr;
 
 
 	/*

commit ff6bf6f18f50cdea561621ac482432d8a14c7415

server/udp: zombie-reaper: fix potential data race with epoll threads
Calling `close_udp_session()` from zombie reaper thread is the wrong
thing to do. Because it uses `struct epl_thread` buffer, which is
being used by other threads. This leads to data race because there
is no locking mechanism that protects those buffers.

Fix for this is that, allocate separated buffer for zombie reaper
thread so it does not clobber other threads' buffer.

Fixes: f65243c69a7d6fa0233533400ce709fcf5ecd5ff ("server/udp: implement zombie reaper thread")
Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

src/teavpn2/server/linux/udp_epoll.c
@@ -920,6 +920,21 @@ out:
 }
 
 
+static int zombie_close_sess(struct srv_udp_state *state, struct udp_sess *sess)
+{
+	size_t send_len;
+	struct srv_pkt *srv_pkt = &state->zr.pkt->srv;
+
+	prl_notice(2, "[zombie reaper] Closing session " PRWIU "...", W_IU(sess));
+	if (sess->ipv4_iff != 0)
+		del_ipv4_route_map(state->ipv4_map, sess->ipv4_iff);
+
+	send_len = srv_pprep(srv_pkt, TSRV_PKT_CLOSE, 0, 0);
+	send_to_client(&state->epl_threads[0], sess, srv_pkt, send_len);
+	return put_udp_session(state, sess);
+}
+
+
 static void zombie_chk_authenticated_sess(struct srv_udp_state *state,
 					  struct udp_sess *sess,
 					  time_t time_diff)
@@ -930,12 +945,15 @@ static void zombie_chk_authenticated_sess(struct srv_udp_state *state,
 	const time_t kill_timeout = UDP_SESS_TIMEOUT * 2;
 
 	if (time_diff > kill_timeout) {
-		close_udp_session(&state->epl_threads[0], sess);
+		zombie_close_sess(state, sess);
 		return;
 	}
 
 	if (time_diff < UDP_SESS_TIMEOUT)
 		return;
+
+	prl_notice(3, "[zombie reaper] Sending request sync packet to " PRWIU,
+		   W_IU(sess));
 
 	send_len = srv_pprep(srv_pkt, TSRV_PKT_REQSYNC, 0, 0);
 	send_to_client(thread, sess, srv_pkt, send_len);
@@ -946,15 +964,10 @@ static void zombie_chk_unauthenticated_sess(struct srv_udp_state *state,
 					    struct udp_sess *sess,
 					    time_t time_diff)
 {
-	size_t send_len;
-	struct srv_pkt *srv_pkt = &state->zr.pkt->srv;
-	struct epl_thread *thread = &state->epl_threads[0];
 	const time_t kill_timeout = UDP_SESS_TIMEOUT;
 
-	if (time_diff > kill_timeout) {
-		send_len = srv_pprep(srv_pkt, TSRV_PKT_REQSYNC, 0, 0);
-		send_to_client(thread, sess, srv_pkt, send_len);
-	}
+	if (time_diff > kill_timeout)
+		zombie_close_sess(state, sess);
 }
 
 
@@ -962,6 +975,9 @@ static void _run_zombie_reaper_thread(struct srv_udp_state *state)
 {
 	uint16_t i, j, max_conn = state->cfg->sock.max_conn;
 	struct udp_sess *sess, *sess_arr = state->sess_arr;
+
+	pr_debug("[zombie reaper] Online client(s): %hu",
+		 atomic_load(&state->n_on_sess));
 
 	for (j = i = 0; i < max_conn; i++) {
 		time_t time_diff;
@@ -1120,7 +1136,7 @@ static void close_epoll_fds(struct srv_udp_state *state)
 }
 
 
-static void close_client_sess(struct srv_udp_state *state)
+static void close_all_client_sess(struct srv_udp_state *state)
 {
 	struct udp_sess *sess_arr = state->sess_arr;
 	uint16_t i, max_conn = state->cfg->sock.max_conn;
@@ -1160,7 +1176,7 @@ static void destroy_epoll(struct srv_udp_state *state)
 		goto thread_wont_exit;
 
 	close_epoll_fds(state);
-	close_client_sess(state);
+	close_all_client_sess(state);
 	free_pkt_buffer(state);
 	al64_free(state->epl_threads);
 	return;

src/teavpn2/server/linux/udp_session.c
@@ -184,17 +184,20 @@ out:
 }
 
 
-int put_udp_session(struct srv_udp_state *state, struct udp_sess *sess)
+int put_udp_session(struct srv_udp_state *state, struct udp_sess *cur_sess)
 	__acquires(&state->sess_stk_lock)
 	__releases(&state->sess_stk_lock)
 {
 	int ret = 0;
 	mutex_lock(&state->sess_stk_lock);
-	BUG_ON(bt_stack_push(&state->sess_stk, sess->idx) == -1);
+	if (!atomic_load(&cur_sess->is_connected) && cur_sess->src_addr == 0)
+		goto out;
+	BUG_ON(bt_stack_push(&state->sess_stk, cur_sess->idx) == -1);
 	if (state->sess_map)
-		ret = remove_sess_from_bkt(state, sess);
-	reset_udp_session(sess, sess->idx);
+		ret = remove_sess_from_bkt(state, cur_sess);
+	reset_udp_session(cur_sess, cur_sess->idx);
 	mutex_unlock(&state->sess_stk_lock);
 	atomic_fetch_sub(&state->n_on_sess, 1);
+out:
 	return ret;
 }