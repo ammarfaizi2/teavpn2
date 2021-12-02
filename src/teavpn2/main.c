540299265a5e3f1ad1d77c0b5cdee94641431f72
Ammar Faizi <ammarfaizi2@gmail.com>
2021-12-03 00:02:42 +0700

    main: Refactor entry app
    Refactor and change the help text. No functional changes are intended.
    
    Signed-off-by: Ammar Faizi <ammarfaizi2@gmail.com>

--- src/teavpn2/main.c
+++ src/teavpn2/main.c
@@ -6,28 +6,25 @@
 #include <stdio.h>
 #include <teavpn2/common.h>
 
-
 void show_version(void)
 {
-	puts("TeaVPN2 " TEAVPN2_VERSION);
+	puts("----------------------------------------------------------");
+	puts("TeaVPN2 " TEAVPN2_VERSION "\n");
 	puts("Copyright (C) 2021 Ammar Faizi\n"
 	     "This is free software; see the source for copying conditions.  There is NO\n"
 	     "warranty; not even for MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.\n");
+	puts("GitHub repo: https://github.com/TeaInside/teavpn2\n");
 }
-
 
 static void show_general_usage(const char *app)
 {
-	printf(" Usage: %s [client|server] [options]\n\n", app);
-	printf(" See:\n");
-	printf("  [Help]\n");
-	printf("    %s server --help\n", app);
+	printf("Usage: %s [client|server] [options]\n\n", app);
+	printf("  For help, see:\n");
 	printf("    %s client --help\n", app);
+	printf("    %s server --help\n", app);
 	printf("\n");
-	printf("  [Version]\n");
-	printf("    %s --version, -V\n\n", app);
+	show_version();
 }
-
 
 static int run_teavpn2(int argc, char *argv[])
 {
@@ -52,15 +49,28 @@ static int run_teavpn2(int argc, char *argv[])
 	return 1;
 }
 
+static void fix_stdio_buffer(void)
+{
+	/*
+	 * Explicitly set the buffer to _IOLBF (line buffered).
+	 * This is important to make sure the buffer is always
+	 * line buffered, especially when we redirect the output
+	 * input a file, like:
+	 *   ./teavpn2 server -c config.ini >> server.log 2>&1;
+	 *
+	 * Without setting it up to `_IOLBF` here, the buffer
+	 * will be full buffered when the above redirect is done.
+	 */
+	if (setvbuf(stdout, NULL, _IOLBF, 2048))
+		printf("Cannot set stdout buffer: %s\n", strerror(errno));
+	if (setvbuf(stderr, NULL, _IOLBF, 2048))
+		printf("Cannot set stderr buffer: %s\n", strerror(errno));
+}
 
 int main(int argc, char *argv[])
 {
-	if (setvbuf(stdout, NULL, _IOLBF, 2048))
-		printf("Cannot set stdout buffer: %s\n", strerror(errno));
 
-	if (setvbuf(stderr, NULL, _IOLBF, 2048))
-		printf("Cannot set stderr buffer: %s\n", strerror(errno));
-
+	fix_stdio_buffer();
 	if (argc == 1) {
 		show_general_usage(argv[0]);
 		return 0;
@@ -73,6 +83,5 @@ int main(int argc, char *argv[])
 		return -ret;
 	}
 #endif
-
 	return run_teavpn2(argc, argv);
 }
